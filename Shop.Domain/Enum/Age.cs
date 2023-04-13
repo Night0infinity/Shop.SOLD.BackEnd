10b7749262765dd9a925af3dc4907690b4f2df1e
Night0infinity <liubomyr.kurylo.ik.2022@lpnu.ua>
2023-04-13 13:45:42 +0300

    Add project files.

---
+++ Shop.Domain/Enum/Age.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace Shop.Domain.Enum
+{
+    public enum Age
+    {
+        [Display(Name = "Від 0 до 3 років")]
+        Newborn = 0,
+        [Display(Name = "Дошкільне")]
+        PreSchool = 1,
+        [Display(Name = "Шкільне")]
+        School = 2,
+        [Display(Name = "Підліткове")]
+        Teen = 3,
+        [Display(Name = "Доросле")]
+        Adult = 4
+    }
+}
---
+++ Shop.Domain/Enum/ClothesTypes.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace Shop.Domain.Enum
+{
+    public enum ClothesTypes
+    {
+        [Display(Name = "Взуття")]
+        Shoes = 0,
+        [Display(Name = "Верхній одяг")]
+        Outerwear = 1,
+        [Display(Name = "Нижня білизна")]
+        Underwear = 2,
+        [Display(Name = "Костюми")]
+        Costume = 3,
+        [Display(Name = "Сорочки")]
+        Shirts = 4,
+        [Display(Name = "Футболки")]
+        Tshirts = 5,
+        [Display(Name = "Штани")]
+        Pants = 6,
+        [Display(Name = "Спортивний")]
+        Sport = 7
+
+    }
+}
